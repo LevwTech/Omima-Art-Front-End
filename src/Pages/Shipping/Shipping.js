eed99ae79215f5930b76150e0f731a63b6725fdd
levvw <abdelraahmanmostafa@gmail.com>
2022-03-22 14:55:31 +0200

    changed egp usd

--- src/Pages/Shipping/Shipping.js
+++ src/Pages/Shipping/Shipping.js
@@ -212,7 +212,7 @@ const countries = [
   "Zimbabwe",
 ];
 function Shipping(props) {
-  const USD = 18.22;
+  const USD = 18.5;
   const { loginWithRedirect, logout, isAuthenticated, user, isLoading } =
     useAuth0();
 
