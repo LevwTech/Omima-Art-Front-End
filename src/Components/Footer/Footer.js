bfddc5507f9a3a66b84277ba9dd1111a05f70e8f
levvw <abdelraahmanmostafa@gmail.com>
2022-03-25 20:00:19 +0200

    Fixed Fix Footer not sticking at bottom ⚡

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,52 +8,54 @@ function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <React.Fragment>
-      <footer className={classes.footer}>
-        <div className={classes.section}>
-          <h1>Menu</h1>
+      <div className={classes.footercontainer}>
+        <footer className={classes.footer}>
+          <div className={classes.section}>
+            <h1>Menu</h1>
 
-          <Link to="/" style={{ textDecoration: "none", color: "#ffffff" }}>
-            <div>Gallery</div>
-          </Link>
-          <Link
-            to="/exhibitions"
-            style={{ textDecoration: "none", color: "#ffffff" }}
-          >
-            <div>Exhibitions</div>
-          </Link>
-          <Link
-            to="/contact"
-            style={{ textDecoration: "none", color: "#ffffff" }}
-          >
-            <div>Contact</div>
-          </Link>
-          <Link
-            to="/about"
-            style={{ textDecoration: "none", color: "#ffffff" }}
-          >
-            <div>About</div>
-          </Link>
-        </div>
-        <div className={classes.section}>
-          <h1>Follow me!</h1>
-          <a
-            href="https://www.facebook.com/OA-Art-Gallery-1475989659102601"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={FBIcon} alt="facebook" />
-          </a>
-          <a
-            href="https://www.instagram.com/omima_aboelnasr_art/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={instaIcon} alt="instagram" />
-          </a>
-        </div>
-      </footer>
+            <Link to="/" style={{ textDecoration: "none", color: "#ffffff" }}>
+              <div>Gallery</div>
+            </Link>
+            <Link
+              to="/exhibitions"
+              style={{ textDecoration: "none", color: "#ffffff" }}
+            >
+              <div>Exhibitions</div>
+            </Link>
+            <Link
+              to="/contact"
+              style={{ textDecoration: "none", color: "#ffffff" }}
+            >
+              <div>Contact</div>
+            </Link>
+            <Link
+              to="/about"
+              style={{ textDecoration: "none", color: "#ffffff" }}
+            >
+              <div>About</div>
+            </Link>
+          </div>
+          <div className={classes.section}>
+            <h1>Follow me!</h1>
+            <a
+              href="https://www.facebook.com/OA-Art-Gallery-1475989659102601"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={FBIcon} alt="facebook" />
+            </a>
+            <a
+              href="https://www.instagram.com/omima_aboelnasr_art/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={instaIcon} alt="instagram" />
+            </a>
+          </div>
+        </footer>
 
-      <div className={classes.rights}> @ {currentYear} Omima Art Gallery</div>
+        <div className={classes.rights}> @ {currentYear} Omima Art Gallery</div>
+      </div>
     </React.Fragment>
   );
 }
