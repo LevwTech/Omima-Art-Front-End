4ab774acd71f22206d8a0432a36ec1f778925f56
levvw <abdelraahmanmostafa@gmail.com>
2022-11-03 09:33:26 +0200

    changed onscroll params

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -9,7 +9,7 @@ function Category({ category }) {
   useEffect(() => {
     const scrollPosition = sessionStorage.getItem("scrollPosition");
     if (scrollPosition) {
-      window.scrollTo(0, parseInt(scrollPosition, 10));
+      window.scrollTo(0, parseInt(scrollPosition));
       sessionStorage.removeItem("scrollPosition");
     }
   }, []);
